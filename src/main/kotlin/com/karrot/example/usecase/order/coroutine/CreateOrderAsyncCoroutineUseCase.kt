d94d2bed28f10cd07344602172093bf1c977c87b
grizz <grizz@daangn.com>
2021-12-20 19:25:35 +0900

    feat: add async example

---
+++ src/main/kotlin/com/karrot/example/usecase/order/coroutine/CreateOrderAsyncCoroutineUseCase.kt
@@ -0,0 +1,63 @@
+package com.karrot.example.usecase.order.coroutine
+
+import com.karrot.example.entity.order.Order
+import com.karrot.example.repository.account.UserAsyncRepository
+import com.karrot.example.repository.catalog.ProductAsyncRepository
+import com.karrot.example.repository.order.OrderAsyncRepository
+import com.karrot.example.repository.shipment.AddressAsyncRepository
+import com.karrot.example.repository.store.StoreAsyncRepository
+import com.karrot.example.usecase.order.CreateOrderUseCaseBase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.jdk9.awaitLast
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.rx3.awaitSingle
+
+class CreateOrderAsyncCoroutineUseCase(
+    private val userRepository: UserAsyncRepository,
+    private val addressRepository: AddressAsyncRepository,
+    private val productRepository: ProductAsyncRepository,
+    private val storeRepository: StoreAsyncRepository,
+    private val orderRepository: OrderAsyncRepository,
+) : CreateOrderUseCaseBase() {
+    data class InputValues(
+        val userId: String,
+        val productIds: List<String>,
+    )
+
+    suspend fun execute(inputValues: InputValues): Order {
+        val (userId, productIds) = inputValues
+
+        // 1. 구매자 조회
+        val buyer = userRepository.findUserByIdAsMaybe(userId).awaitSingle()
+
+        // 2. 주소 조회 및 유효성 체크
+        val addressDeferred = CoroutineScope(Dispatchers.IO).async {
+            addressRepository.findAddressByUserAsPublisher(buyer)
+                .awaitLast()
+        }
+
+        // 3. 상품들 조회
+        val products = productRepository.findAllProductsByIdsAsFlux(productIds).asFlow().toList()
+        check(products.isNotEmpty())
+
+        // 4. 스토어 조회
+        val storesDeferred = CoroutineScope(Dispatchers.IO).async {
+            storeRepository.findStoresByProductsAsMulti(products).asFlow().toList()
+        }
+
+        val address = addressDeferred.await()
+        val stores = storesDeferred.await()
+
+        checkValidRegion(address)
+        check(stores.isNotEmpty())
+
+        // 5. 주문 생성
+        val order = orderRepository.createOrderAsFuture(buyer, products, stores, address).await()
+
+        return order
+    }
+}
---
+++ src/main/kotlin/com/karrot/example/usecase/order/coroutine/CreateOrderErrorCoroutineUseCase.kt
@@ -0,0 +1,56 @@
+package com.karrot.example.usecase.order.coroutine
+
+import com.karrot.example.entity.order.Order
+import com.karrot.example.repository.account.UserAsyncRepository
+import com.karrot.example.repository.catalog.ProductAsyncRepository
+import com.karrot.example.repository.order.OrderAsyncRepository
+import com.karrot.example.repository.shipment.AddressAsyncRepository
+import com.karrot.example.repository.store.StoreAsyncRepository
+import com.karrot.example.usecase.order.CreateOrderUseCaseBase
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.jdk9.awaitLast
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.rx3.awaitSingle
+
+class CreateOrderErrorCoroutineUseCase(
+    private val userRepository: UserAsyncRepository,
+    private val addressRepository: AddressAsyncRepository,
+    private val productRepository: ProductAsyncRepository,
+    private val storeRepository: StoreAsyncRepository,
+    private val orderRepository: OrderAsyncRepository,
+) : CreateOrderUseCaseBase() {
+    data class InputValues(
+        val userId: String,
+        val productIds: List<String>,
+    )
+
+    suspend fun execute(inputValues: InputValues): Order {
+        val (userId, productIds) = inputValues
+
+        // 1. 구매자 조회
+        val buyer = try {
+            userRepository.findUserByIdAsMaybe(userId).awaitSingle()
+        } catch (e: Exception) {
+            throw NoSuchElementException("no such user")
+        }
+
+        // 2. 주소 조회 및 유효성 체크
+        val address = addressRepository.findAddressByUserAsPublisher(buyer)
+            .awaitLast()
+        checkValidRegion(address)
+
+        // 3. 상품들 조회
+        val products = productRepository.findAllProductsByIdsAsFlux(productIds).asFlow().toList()
+        check(products.isNotEmpty())
+
+        // 4. 스토어 조회
+        val stores = storeRepository.findStoresByProductsAsMulti(products).asFlow().toList()
+        check(stores.isNotEmpty())
+
+        // 5. 주문 생성
+        val order = orderRepository.createOrderAsFuture(buyer, products, stores, address).await()
+
+        return order
+    }
+}
